6c15b768ab9ed9452189ee627dbe02a1e6eadada
Brendan Holt <brendanholt@users.noreply.github.com>
2016-10-28 23:20:07 -0700

    Added Qt Designer forms and UI compile scripts.

---
+++ design/forms/build_wiretapBrowser.bat
@@ -0,0 +1,2 @@
+REM Refreshes the generated Wiretap Browser Qt UI class.
+pyside-uic wiretapBrowserB02.ui --output=../../WiretapBrowserUI.py
---
+++ design/forms/build_wiretapShotProcessor.bat
@@ -0,0 +1,3 @@
+pyside-uic wiretapShotProcessorA04.ui --output=../../plugins/FnWiretapShotProcessorUI.py
+
+REM Must override the image path to resources/images/cbsd_logo.png
